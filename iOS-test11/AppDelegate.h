68f942a22af3ef6dd42519f347059838c78ef8d1
Ibokan <ibokan@purity-pro.local>
2015-11-26 10:30:52 +0800

    Initial Commit

---
+++ iOS-test11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-test11
+//
+//  Created by Ibokan on 15/11/26.
+//  Copyright © 2015年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-test11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-test11
+//
+//  Created by Ibokan on 15/11/26.
+//  Copyright © 2015年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
